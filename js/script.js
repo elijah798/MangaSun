e2d93240a5490927f8e13fabfd8228f9c2ac0406
elijah798 <46266593+elijah798@users.noreply.github.com>
2020-10-15 10:26:40 -0700

    Added Files
    Added all files.

---
+++ js/script.js
@@ -0,0 +1,91 @@
+const modal = document.getElementById('modal');
+const pageImg = document.getElementById('page');
+let page = 1;
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            previousPage();
+        } else {
+            nextPage();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            unfade(modal);
+        } else { 
+            fade(modal);
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+function nextPage() {
+    page += 1;
+    pageImg.src = `/images/Komi-sama/0270-00${page}.png`
+    console.log(page);
+}
+
+function previousPage() {
+    if(page > 1){
+        page -= 1;
+        pageImg.src = `/images/Komi-sama/0270-00${page}.png`
+        console.log(page);
+    }
+}
+
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'flex';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
